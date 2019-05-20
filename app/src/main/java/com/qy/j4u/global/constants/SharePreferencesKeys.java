2d792cad6e0bc6848a6b3b215ef75a8a2a033977
qianyue <1974765766@qq.com>
2019-05-20 22:16:17 +0800

    改成interface

--- app/src/main/java/com/qy/j4u/global/constants/SharePreferencesKeys.java
+++ app/src/main/java/com/qy/j4u/global/constants/SharePreferencesKeys.java
@@ -1,20 +1,20 @@
 package com.qy.j4u.global.constants;
 
-public class SharePreferencesKeys {
+public interface SharePreferencesKeys {
 
     /**
      * 默认的xml名字
      */
-    public static final String DEFAULT_NAME = "j4u";
+    String DEFAULT_NAME = "j4u";
 
     /**
      * 获取uuid的
      */
-    public static final String KEY_UUID = "device_uuid";
-    public static final String KEY_USER_NAME = "user_name";
-    public static final String KEY_USER_ID = "user_id";
-    public static final String KEY_USER_PHONE = "user_phone";
-    public static final String KEY_USER_TOKEN = "user_token";
-    public static final String KEY_USER_IT_CATEGORIES = "user_it_categories";
+    String KEY_UUID = "device_uuid";
+    String KEY_USER_NAME = "user_name";
+    String KEY_USER_ID = "user_id";
+    String KEY_USER_PHONE = "user_phone";
+    String KEY_USER_TOKEN = "user_token";
+    String KEY_USER_IT_CATEGORIES = "user_it_categories";
 
 }
