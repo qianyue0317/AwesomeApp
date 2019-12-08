40de7b54f713a183325d5eb7c19665da92f09d61
qianyue <1974765766@qq.com>
2019-12-08 21:10:38 +0800

    添加通过coap控制pc

---
+++ app/src/main/java/com/qy/j4u/app/pccontroller/CoapControlActivity.java
@@ -0,0 +1,38 @@
+package com.qy.j4u.app.pccontroller;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.qy.j4u.R;
+import com.qy.j4u.utils.coap.Client;
+
+import org.eclipse.californium.core.CoapResponse;
+
+public class CoapControlActivity extends AppCompatActivity implements Client.Callback {
+
+    private static final String TAG = CoapControlActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coap);
+        findViewById(R.id.btn).setOnClickListener(v -> {
+            Client client = Client.getIns();
+            client.setUri("192.168.1.104:5683/basic");
+            client.get(CoapControlActivity.this);
+        });
+    }
+
+    @Override
+    public void onResponse(CoapResponse response) {
+        Log.i(TAG, "onResponse: " + response);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        Log.i(TAG, "onError: " + e);
+    }
+}
